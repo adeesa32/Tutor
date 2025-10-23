abb282b6de9beb02a154eae2f6f7449474730686
Adelina Escobar Acevedo <57870687+adeesa32@users.noreply.github.com>
2025-10-23 12:29:29 -0600

    Add files via upload

---
+++ src/app/components/activity1/activity1.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataApiService } from 'src/app/services/data-api.service';
+
+@Component({
+  selector: 'app-activity1',
+  templateUrl: './activity1.component.html',
+  styleUrls: ['./activity1.component.css']
+})
+export class Activity1Component implements OnInit {
+  forma: FormGroup;
+  descripcionPreguntas: string[] = [];
+  pregPreguntas: string[] = [];
+  resPreguntas: string[] = [];
+
+  constructor(private fb: FormBuilder, private apiservice: DataApiService, private router: Router) {
+    this.preguntas();
+  }
+
+  ngOnInit(): void {
+    this.getCuestionario();
+  }
+
+  preguntas(): void {
+    this.forma = this.fb.group({
+      q1: ['', , ] ,
+      q2: ['', , ] ,
+      q3: ['', []] ,
+      q4t: ['', , ] ,
+      q4f: ['', , ] ,
+    });
+  }
+
+  guardar(): void {
+    console.log(this.forma);
+  }
+
+  getCuestionario(): void {
+    this.apiservice.getAllCuestionario().subscribe((res: any) => {
+      let cuestionario = [];
+      for(let i = 0; i < res.problema.length; i++){
+        let preguntaObject = {descripcion: res.problema[i].descripcion,
+          preguntas: res.problema[i].preguntas,
+          respuestas: res.problema[i].respuestas
+        };
+        this.descripcionPreguntas.push(res.problema[i].descripcion);
+        this.pregPreguntas.push(res.problema[i].preguntas);
+        this.resPreguntas.push(res.problema[i].respuestas);
+      }
+      console.log(this.descripcionPreguntas);
+      console.log(this.pregPreguntas);
+      console.log(this.resPreguntas);
+    });
+  }
+}
